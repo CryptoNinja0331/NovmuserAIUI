99f18f86c26e41ec74dde7a45b0079d43f4b6cb2
CryptoNinja0331 <luckydev0331@gmail.com>
2024-04-02 22:46:16 +0600

    bug fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/api/webhook/clerk"],
+  publicRoutes: ["/api/webhooks/clerk"],
 });
 
 export const config = {
