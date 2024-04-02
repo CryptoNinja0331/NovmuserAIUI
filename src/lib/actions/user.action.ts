05d4d70fa1d8fd22558c206782e3a706063361b2
CryptoNinja0331 <luckydev0331@gmail.com>
2024-04-02 22:30:12 +0600

    store data in mongodb

---
+++ src/lib/actions/user.action.ts
@@ -0,0 +1,14 @@
+"use server";
+
+import { connect } from "@/lib/db";
+import User from "../models/user.model";
+
+export async function createUser(user: any) {
+  try {
+    await connect();
+    const newUser = await User.create(user);
+    return JSON.parse(JSON.stringify(newUser));
+  } catch (error) {
+    console.log(error);
+  }
+}
