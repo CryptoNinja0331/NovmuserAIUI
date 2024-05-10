2895196c1ab3e72c69089cf33c1049f1f44089c4
CryptoNinja0331 <luckydev0331@gmail.com>
2024-05-10 19:36:22 +0600

    db change

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -22,7 +22,7 @@ export const connect = async () => {
   cached.promise =
     cached.promise ||
     mongoose.connect(MONGODB_URL, {
-      dbName: "clerk-next14-db",
+      dbName: "novmuserai_db",
       bufferCommands: false,
       connectTimeoutMS: 30000,
     });
