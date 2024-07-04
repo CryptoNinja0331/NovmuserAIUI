ea8b55c63e3c25ffcf4f15704dabbd5ac22f5f37
CryptoNinja0331 <luckydev0331@gmail.com>
2024-07-04 15:10:01 +0800

    add chapterContext

--- src/app/(dashboard)/novel/[novelId]/chapter/[[...chapterKey]]/context/useChapterContext.tsx
+++ src/app/(dashboard)/novel/[novelId]/chapter/[[...chapterKey]]/context/useChapterContext.tsx
@@ -2,27 +2,27 @@ import React from 'react';
 const initValue = {
 	currentTopicId: '',
 	currentPointerId: '',
-	updateCurrentId: (topicId: string, pointerId: String) => {}
+	updateCurrentId: (topicId: string, pointerId: string) => { }
 }
 const ChapterContext = React.createContext(initValue)
 
 const ChapterProvider = ({ children }: React.PropsWithChildren) => {
 	const [currentTopicId, updateCurrentTopicId] = React.useState('')
 	const [currentPointerId, updateCurrentPointerId] = React.useState('')
-	const updateCurrentId = (topicId, pointerId) => {
+	const updateCurrentId = (topicId: string, pointerId: string) => {
 		console.log(topicId, pointerId)
 		updateCurrentPointerId(pointerId)
 		updateCurrentTopicId(topicId)
 	}
 	return (
 		<ChapterContext.Provider
-			value= {{
+			value={{
 				currentPointerId,
 				currentTopicId,
 				updateCurrentId
 			}}
 		>
-			{ children }
+			{children}
 		</ChapterContext.Provider>
 	)
 }
