0b1639d62964eb335ce5946d49cb9a58afd6191d
CryptoNinja0331 <luckydev0331@gmail.com>
2024-07-20 01:15:53 +0800

    feat(NA-130): init plot card

--- src/components/aiAgent/PlotUi.tsx
+++ src/components/aiAgent/PlotUi.tsx
@@ -1,138 +1,191 @@
-import React from 'react';
-import 'react-vertical-timeline-component/style.min.css';
+import { TPlotBeginning, TPlotOutline } from "@/lib/types/api/agent";
+import { TWsMsgDto } from "@/lib/types/api/websocket";
+import React, { FC } from "react";
+import "react-vertical-timeline-component/style.min.css";
+import { Textarea } from "../ui/textarea";
+import { Card, CardContent, CardTitle } from "../ui/card";
 
-interface Subplot {
-    plot: string;
-    involved_characters: string[];
-    expected_impact: string;
-}
+export type TPlotUiProps = {
+  novelMsg: TWsMsgDto;
+};
 
-interface Beginning {
-    event: string;
-    main_characters: string[];
-    scene: string;
-}
+type TPlotData = {
+  title: string;
+  description?: string;
+  renderer?: React.ReactNode;
+};
 
-interface Development {
-    conflict_expansion: string;
-    inciting_incident: string;
-    subplots: Subplot[];
-}
+const PlotItemCard: FC<TPlotData> = ({ title, description, renderer }) => {
+  return (
+    <Card className="w-[80%] rounded-3xl">
+      <div className="flex flex-row justify-center my-2">
+        <CardTitle className="bg-red-500 text-4xl">{title}</CardTitle>
+      </div>
+      {renderer}
+    </Card>
+  );
+};
 
-interface Climax {
-    key_turn: string;
-    decisions: string;
-    consequences: string;
-}
+const PlotUi: React.FC<TPlotUiProps> = ({ novelMsg }) => {
+  const plotOutlineData: TPlotOutline = React.useMemo(
+    () => novelMsg.msg || {},
+    [novelMsg.msg]
+  );
+  console.log("🚀 PlotUi ~ plotOutlineData:", plotOutlineData);
 
-interface Ending {
-    character_outcomes: string;
-    conflict_resolution: string;
-    loose_ends: string;
-    world_state: string;
-}
+  //   const renderSubplots = (subplots?: Subplot[]) => {
+  //     return subplots?.map((subplot, index) => (
+  //       <div key={index} className="bg-gray-100 rounded-md p-4 shadow-md mb-4">
+  //         <h4 className="text-lg font-semibold mb-2">Plot: {subplot.plot}</h4>
+  //         <p className="mb-2">
+  //           Involved Characters: {subplot.involved_characters.join(", ")}
+  //         </p>
+  //         <p>Expected Impact: {subplot.expected_impact}</p>
+  //       </div>
+  //     ));
+  //   };
 
-interface PlotData {
-    beginning: Beginning;
-    development: Development;
-    climax: Climax;
-    ending: Ending;
-}
-
-interface PlotUiProps {
-    novelMsg: any | null;
-}
-
-const PlotUi: React.FC<PlotUiProps> = ({ novelMsg }) => {
-    const data: PlotData = novelMsg.msg || {};
-
-    const renderSubplots = (subplots?: Subplot[]) => {
-        return subplots?.map((subplot, index) => (
-            <div key={index} className="bg-gray-100 rounded-md p-4 shadow-md mb-4">
-                <h4 className="text-lg font-semibold mb-2">Plot: {subplot.plot}</h4>
-                <p className="mb-2">Involved Characters: {subplot.involved_characters.join(', ')}</p>
-                <p>Expected Impact: {subplot.expected_impact}</p>
-            </div>
-        ));
-    };
-
-    const plotData = [
-        {
-            title: 'Beginning',
-            description: (
-                <div>
-                    <p className="mb-2">Event: {data?.beginning?.event}</p>
-                    <p className="mb-2">
-                        Main Characters:
-                        {data?.beginning?.main_characters?.map((item, index) => (
-                            <span key={index} className="inline-block bg-gray-200 rounded-full px-2 py-1 text-sm font-semibold mr-2">
-                                {item}
-                            </span>
-                        ))}
-                    </p>
-                    <p>Scene: {data?.beginning?.scene}</p>
-                </div>
-            ),
-        },
-        {
-            title: 'Development',
-            description: (
-                <div>
-                    <p className="mb-2">Conflict Expansion: {data?.development?.conflict_expansion}</p>
-                    <p className="mb-2">Inciting Incident: {data?.development?.inciting_incident}</p>
-                    <div className="mt-4">
-                        <h3 className="text-lg font-semibold mb-2">Subplots:</h3>
-                        {renderSubplots(data?.development?.subplots)}
-                    </div>
-                </div>
-            ),
-        },
-        {
-            title: 'Climax',
-            description: (
-                <div>
-                    <p className="mb-2">Key Turn: {data?.climax?.key_turn}</p>
-                    <p className="mb-2">Decisions: {data?.climax?.decisions}</p>
-                    <p>Consequences: {data?.climax?.consequences}</p>
-                </div>
-            ),
-        },
-        {
-            title: 'Ending',
-            description: (
-                <div>
-                    <p className="mb-2">Character Outcomes: {data?.ending?.character_outcomes}</p>
-                    <p className="mb-2">Conflict Resolution: {data?.ending?.conflict_resolution}</p>
-                    <p className="mb-2">Loose Ends: {data?.ending?.loose_ends}</p>
-                    <p>World State: {data?.ending?.world_state}</p>
-                </div>
-            ),
-        },
+  const plotData = React.useMemo<TPlotData[]>(() => {
+    const { beginning, development, climax, ending } = plotOutlineData;
+    return [
+      {
+        title: "Begining",
+        // TODO 2024-07-20 Replace description
+        description: "Begining",
+        renderer: (
+          <div>
+            <CardContent>
+              <CardTitle>Scene:</CardTitle>
+              <Textarea defaultValue={beginning?.scene ?? ""} />
+            </CardContent>
+            <CardContent>
+              <CardTitle>Event:</CardTitle>
+              <Textarea defaultValue={beginning?.event ?? ""} />
+            </CardContent>
+          </div>
+        ),
+      },
+      {
+        title: "Development",
+        description: "Development",
+        renderer: <></>,
+      },
+      {
+        title: "Climax",
+        description: "Climax",
+        renderer: <></>,
+      },
+      {
+        title: "Ending",
+        description: "Ending",
+        renderer: <></>,
+      },
     ];
+  }, [plotOutlineData]);
 
-    return (
-        <div>
-            <h2 className="text-2xl font-bold mb-4">Plot</h2>
-            <section className="timeline-landing">
-                <div className="row container">
-                    <div className="timeline">
-                        <div className="timeline-line" />
-                        {plotData.map((event, index) => (
-                            <div key={index} className="timeline-event">
-                                <div className="timeline-event-content bg-white rounded-lg shadow-md p-6">
-                                    <div className="event-details">
-                                        <h3 className="text-xl font-semibold mb-4">{event.title}</h3>
-                                        {event.description}
-                                    </div>
-                                </div>
-                                {index < plotData.length - 1 && <div className="timeline-connector" />}
-                            </div>
-                        ))}
-                    </div>
-                </div>
-            </section>
+  //   const plotData: {
+  //     title: string;
+  //     description: React.ReactNode;
+  //   } = [
+  //     {
+  //       title: "Beginning",
+  //       description: (
+  //         <div>
+  //           <p className="mb-2">Event: {data?.beginning?.event}</p>
+  //           <p className="mb-2">
+  //             Main Characters:
+  //             {data?.beginning?.main_characters?.map((item, index) => (
+  //               <span
+  //                 key={index}
+  //                 className="inline-block bg-gray-200 rounded-full px-2 py-1 text-sm font-semibold mr-2"
+  //               >
+  //                 {item}
+  //               </span>
+  //             ))}
+  //           </p>
+  //           <p>Scene: {data?.beginning?.scene}</p>
+  //         </div>
+  //       ),
+  //     },
+  //     {
+  //       title: "Development",
+  //       description: (
+  //         <div>
+  //           <p className="mb-2">
+  //             Conflict Expansion: {data?.development?.conflict_expansion}
+  //           </p>
+  //           <p className="mb-2">
+  //             Inciting Incident: {data?.development?.inciting_incident}
+  //           </p>
+  //           <div className="mt-4">
+  //             <h3 className="text-lg font-semibold mb-2">Subplots:</h3>
+  //             {renderSubplots(data?.development?.subplots)}
+  //           </div>
+  //         </div>
+  //       ),
+  //     },
+  //     {
+  //       title: "Climax",
+  //       description: (
+  //         <div>
+  //           <p className="mb-2">Key Turn: {data?.climax?.key_turn}</p>
+  //           <p className="mb-2">Decisions: {data?.climax?.decisions}</p>
+  //           <p>Consequences: {data?.climax?.consequences}</p>
+  //         </div>
+  //       ),
+  //     },
+  //     {
+  //       title: "Ending",
+  //       description: (
+  //         <div>
+  //           <p className="mb-2">
+  //             Character Outcomes: {data?.ending?.character_outcomes}
+  //           </p>
+  //           <p className="mb-2">
+  //             Conflict Resolution: {data?.ending?.conflict_resolution}
+  //           </p>
+  //           <p className="mb-2">Loose Ends: {data?.ending?.loose_ends}</p>
+  //           <p>World State: {data?.ending?.world_state}</p>
+  //         </div>
+  //       ),
+  //     },
+  //   ];
+
+  return (
+    <div>
+      <h2 className="text-2xl font-bold mb-4">Plot</h2>
+      <section className="timeline-landing">
+        <div className="row container">
+          <div className="timeline">
+            <div className="timeline-line" />
+            {plotData.map((event, index) => (
+              //   <div key={index} className="timeline-event">
+              //     <div className="timeline-event-content bg-white rounded-lg shadow-md p-6">
+              //       <div className="event-details">
+              //         <h3 className="text-xl font-semibold mb-4">
+              //           {event.title}
+              //         </h3>
+              //         {event.description}
+              //       </div>
+              //     </div>
+              //     {index < plotData.length - 1 && (
+              //       <div className="timeline-connector" />
+              //     )}
+              //   </div>
+              <PlotItemCard
+                key={index}
+                {...{
+                  title: event.title,
+                  description: event.description,
+                  renderer: event.renderer,
+                }}
+              />
+            ))}
+          </div>
         </div>
-    );
+      </section>
+    </div>
+  );
 };
 
 export default PlotUi;
--- src/components/aiAgent/WorldViewUi.tsx
+++ src/components/aiAgent/WorldViewUi.tsx
@@ -1,61 +1,74 @@
-import React from 'react';
+import React from "react";
 import { FaRegEdit } from "react-icons/fa";
-import { FormGroup } from '../ui/form-group';
+import { FormGroup } from "../ui/form-group";
 
 interface WorldViewUiProps {
-    novelMsg: any | null;
+  novelMsg: any | null;
 }
 
 const WorldViewUi: React.FC<WorldViewUiProps> = ({ novelMsg }) => {
-    const { novel_type, novel_style, point_of_view, geography, politics, economy, culture, history } = novelMsg.msg;
+  const {
+    novel_type,
+    novel_style,
+    point_of_view,
+    geography,
+    politics,
+    economy,
+    culture,
+    history,
+  } = novelMsg.msg;
 
-    return (
-        <div>
-            <div className='flex items-center justify-between my-4 border-b-2 pb-2 border-dashed border-[#eaaaff33]'>
-                <h1 className='text-violet-gradient text-xl mb-2 font-semibold '>World View</h1>
-                <FaRegEdit className='text-[#B997E3] text-xl cursor-pointer' />
-            </div>
+  return (
+    <div>
+      <div className="flex items-center justify-between my-4 border-b-2 pb-2 border-dashed border-[#eaaaff33]">
+        <h1 className="text-violet-gradient text-xl mb-2 font-semibold ">
+          World View
+        </h1>
+        <FaRegEdit className="text-[#B997E3] text-xl cursor-pointer" />
+      </div>
 
-
-            <div className='flex gap-8 justify-between items-center w-full mt-5'>
-                <div className='w-1/2'>
-                    <FormGroup label="Novel Type" value={novel_type} className="mb-4" />
-                </div>
-                <div className='w-1/2'>
-                    <FormGroup label="Novel Style" value={novel_style} className="mb-4" />
-                </div>
-            </div>
-
-            <div className='mt-4'>
-                <FormGroup label="Point Of View" value={point_of_view} className="mb-4" />
-            </div>
-
-            <div className='my-8 border-b-2 pb-2 border-dashed border-[#eaaaff33]'>
-            </div>
-
-            <div className='flex gap-8 justify-between items-center w-full mt-5'>
-                <div className='w-1/2'>
-                    <FormGroup label="Geography" value={geography} className="mb-4" />
-                </div>
-                <div className='w-1/2'>
-                    <FormGroup label="Politics" value={politics} className="mb-4" />
-                </div>
-            </div>
-
-            <div className='mt-4'>
-                <FormGroup label="Economy" value={economy} className="mb-4" />
-            </div>
-
-            <div className='flex gap-8 justify-between items-center w-full mt-5'>
-                <div className='w-1/2'>
-                    <FormGroup label="Culture" value={culture} className="mb-4" />
-                </div>
-                <div className='w-1/2'>
-                    <FormGroup label="History" value={history} className="mb-4" />
-                </div>
-            </div>
+      <div className="flex gap-8 justify-between items-center w-full mt-5">
+        <div className="w-1/2">
+          <FormGroup label="Novel Type" value={novel_type} className="mb-4" />
         </div>
-    );
+        <div className="w-1/2">
+          <FormGroup label="Novel Style" value={novel_style} className="mb-4" />
+        </div>
+      </div>
+
+      <div className="mt-4">
+        <FormGroup
+          label="Point Of View"
+          value={point_of_view}
+          className="mb-4"
+        />
+      </div>
+
+      <div className="my-8 border-b-2 pb-2 border-dashed border-[#eaaaff33]"></div>
+
+      <div className="flex gap-8 justify-between items-center w-full mt-5">
+        <div className="w-1/2">
+          <FormGroup label="Geography" value={geography} className="mb-4" />
+        </div>
+        <div className="w-1/2">
+          <FormGroup label="Politics" value={politics} className="mb-4" />
+        </div>
+      </div>
+
+      <div className="mt-4">
+        <FormGroup label="Economy" value={economy} className="mb-4" />
+      </div>
+
+      <div className="flex gap-8 justify-between items-center w-full mt-5">
+        <div className="w-1/2">
+          <FormGroup label="Culture" value={culture} className="mb-4" />
+        </div>
+        <div className="w-1/2">
+          <FormGroup label="History" value={history} className="mb-4" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default WorldViewUi;
