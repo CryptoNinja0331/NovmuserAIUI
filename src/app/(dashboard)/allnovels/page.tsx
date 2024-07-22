1129d1f788fe5ca1d76d9c35868b6dd5b0d63263
CryptoNinja0331 <luckydev0331@gmail.com>
2024-07-22 16:00:15 +0800

    feat(NA-131): chapter outline agent ui

--- src/app/(dashboard)/allnovels/page.tsx
+++ src/app/(dashboard)/allnovels/page.tsx
@@ -6,6 +6,10 @@ import NovelPage from "./_components/NovelPage";
 const page = async ({ searchParams }: { searchParams: any }) => {
   const currentPage = Number(searchParams.page ? searchParams.page : 1);
   const response = await getAllNovels(currentPage);
+  console.log(
+    "🚀 ~ page ~ response?.data?.total_records:",
+    response?.data?.total_records ?? 0
+  );
 
   return (
     <NovelPage novelPageRespDto={response?.data} currentPage={currentPage} />
